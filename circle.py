4b740c12522b40bd52b43d04b5e8f02565d58531
SK386 <TK.O386@proton.me>
2023-10-26 19:12:08 -0300

    Python: initial exercises 1/10

---
+++ circle.py
@@ -0,0 +1,6 @@
+print("Calculate Circle Area")
+
+radius = float(input("Insert radius value: "))
+#radius is r -> 3,14 * r²
+area = ((radius**2)*3.14)
+print(f"the circle area is {area}m²")
---
+++ meter2centimeter.py
@@ -0,0 +1,5 @@
+print("Convert meter to centimeter")
+
+meter = float(input("Insert a meter number: "))
+centimeter = (meter*100)
+print(f"the value in centimeter is: {centimeter}")
